ae3949e0d736a0e55fbe25a6cbd12ac2a42ec865
hanibalnos-ship-it <hanibalnos@gmail.com>
2026-08-29 14:34:03 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose")
-}
-
-android {
-    namespace = "br.com.controlepix"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "br.com.controlepix"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
-    implementation(composeBom)
-
-    implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }

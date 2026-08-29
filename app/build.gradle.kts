7d20aa0b7cc1e4e55bcf799079705eb40bd6d801
hanibalnos-ship-it <hanibalnos@gmail.com>
2026-08-29 14:02:53 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "br.com.controlepix"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "br.com.controlepix"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
